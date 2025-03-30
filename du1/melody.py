0ef8388b18f29b9de8fdea4369d61eb4550c1822
cyprich <cypooriginal@gmail.com>
2025-03-30 15:28:31 +0200

    zakmity done, uloha done

--- du1/melody.py
+++ du1/melody.py
@@ -1,3 +1,5 @@
+import time
+
 from du1.play import *
 from du1.utils import decider
 
@@ -13,15 +15,26 @@ class Melody:
         self._button = Pin(2, Pin.IN, Pin.PULL_UP)
         self._button.irq(handler=lambda x: self.change_volume(), trigger=Pin.IRQ_RISING)
 
+        self._delay = 0.15
+        self._time_before = 0
+
+
     def run(self):
         set_volume(self._volume)
         playsong(melody[self._index - 1])
 
+
     def change_volume(self):
-        self._volume += int(self._max_volume / 4)
-        if self._volume > self._max_volume:
-            self._volume = 0
-        set_volume(self._volume)
+        if time.time() - self._time_before > self._delay:
+            self._time_before = time.time()
+
+            self._volume += int(self._max_volume / 4)
+
+            if self._volume > self._max_volume:
+                self._volume = 0
+
+            set_volume(self._volume)
+            print(f"[Volume: {self._volume // 10}%]")
 
 
 
