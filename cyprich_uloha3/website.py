108d18a96f41a009936b445c78c51b0215462765
cyprich <cypooriginal@gmail.com>
2025-05-05 21:06:32 +0200

    uloha3 parameters funguju

--- cyprich_uloha3/website.py
+++ cyprich_uloha3/website.py
@@ -6,12 +6,16 @@ class Website:
     def __init__(self):
         # creating app
         self._app = Microdot()
+
+        # deleting old template
         os.chdir("templates")
         try:
             os.remove("webpage_html.py")
         except OSError:
             pass
         os.chdir("/")
+
+        # creating new template
         self._webpage = Template("webpage.html")
         Response.default_content_type = "text/html"
 
@@ -33,12 +37,18 @@ class Website:
         async def index(request):
             return self.generate_webpage()
 
+        @self._app.route("/button_1")
+        async def button(request):
+            self._page_vars["variable_parameter_1"] = "Button was pressed"
+            return self.generate_webpage()
+
 
     def generate_webpage(self):
+        print(self._page_vars)
         return self._webpage.render(
-            parameter1 = self._page_vars["variable_parameter_1"],
-            fplf_html = self._page_vars["variable_html"],
-            fplf_css = self._page_vars["variable_css"],
-            fplf_C = self._page_vars["variable_c"],
+            parameter_1 = self._page_vars["variable_parameter_1"],
+            fplf_html=self._page_vars["variable_html"],
+            fplf_css=self._page_vars["variable_css"],
+            fplf_C=self._page_vars["variable_c"],
         )
 
