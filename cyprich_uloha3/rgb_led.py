8121af0d6b769b0bee05b841bd5153fe6bcc9736
cyprich <cypooriginal@gmail.com>
2025-05-07 15:39:26 +0200

    uloha3 ovladanie rgb working

---
+++ cyprich_uloha3/rgb_led.py
@@ -0,0 +1,28 @@
+"""Control RGB LED.
+"""
+
+from machine import PWM
+
+
+class RGBLED:
+    """Class that controls RGB LED."""
+
+    def __init__(self, r_pin_number: int = 21, g_pin_number: int = 11, b_pin_number: int = 10) -> None:
+        """Create instance of the class."""
+        self._max_duty = 65535
+        self._r: PWM = PWM(r_pin_number, freq=120, duty_u16=0)
+        self._g: PWM = PWM(g_pin_number, freq=120, duty_u16=0)
+        self._b: PWM = PWM(b_pin_number, freq=120, duty_u16=0)
+
+    def get(self) -> tuple[float, float, float]:
+        print(self._r.duty_u16())
+        return (
+            self._r.duty_u16() / self._max_duty,
+            self._g.duty_u16() / self._max_duty,
+            self._b.duty_u16() / self._max_duty,
+        )
+
+    def set(self, r_percent: float = 0, g_percent: float = 0, b_percent: float = 0) -> None:
+        self._r.duty_u16(int(r_percent * self._max_duty))
+        self._g.duty_u16(int(g_percent * self._max_duty))
+        self._b.duty_u16(int(b_percent * self._max_duty))
--- cyprich_uloha3/website.py
+++ cyprich_uloha3/website.py
@@ -1,4 +1,6 @@
 import os
+
+from cyprich_uloha3.rgb_led import RGBLED
 from microdot.microdot import Microdot, Response
 from microdot.utemplate import Template
 from cyprich_uloha3.photoresistor import Photoresistor
@@ -20,16 +22,17 @@ class Website:
         self._webpage = Template("webpage.html")
         Response.default_content_type = "text/html"
 
-        # from previous homeworks
+        # peripherals
         self._photoresistor = Photoresistor()
+        self._rgbled = RGBLED()
 
         # defining variables
         self._page_vars = {
             "photoresistor_value": self._photoresistor.get_value(),
             "photoresistor_percent": round(self._photoresistor.get_percent(), 2),
-            "r": 1,
-            "g": 2,
-            "b": 3,
+            "rgb_r": 0,
+            "rgb_g": 0,
+            "rgb_b": 0,
         }
 
         self._endpoints()
@@ -48,7 +51,21 @@ class Website:
             self._page_vars["photoresistor_percent"] = round(self._photoresistor.get_percent(), 2)
             return self.generate_webpage()
 
+        @self._app.route("/rgb_led_form")
+        async def rgb_led_form(request):
+            values = [int(request.args[i]) for i in ["rgb_r", "rgb_g", "rgb_b"]]
+            self._rgbled.set(
+                values[0] / 100,
+                values[1] / 100,
+                values[2] / 100,
+            )
+
+            self._page_vars["rgb_r"] = values[0]
+            self._page_vars["rgb_g"] = values[1]
+            self._page_vars["rgb_b"] = values[2]
+
+            return self.generate_webpage()
+
 
     def generate_webpage(self):
-        print(self._page_vars)
         return self._webpage.render(**self._page_vars)
